7f082483b19d749b28da4951eead488365273d35
Airyque Ervin <airyque@gmail.com>
2017-04-23 19:45:29 -0700

    Will get mentions from a twitter user

--- basic-server.js
+++ basic-server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 8300;
+const Twitter = require('twitter');
+const twitKeys = require('./config/config.js');
+
+const twit = new Twitter({
+  consumer_key: twitKeys.config.csKey, 
+  consumer_secret: twitKeys.config.csSecret,
+  access_token_key: twitKeys.config.accessToken,
+  access_token_secret: twitKeys.config.accessTokenSecret
+});
+
+// console.log(twit)
 
 app.get('/*', (req, res) => {
   res.send('Hello World');
@@ -9,3 +20,34 @@ app.get('/*', (req, res) => {
 const server = app.listen(port, () => {
   console.log('TwitAirBot is listening on port' + port);
 });
+
+
+
+
+const latestMentions = [];
+const idStrings = {};
+
+const getMentions = () => {
+  twit.get('statuses/mentions_timeline.json', { count: 10}, function(err, data, res){
+    if (err) {
+      console.log('ERROR OCCURED WITH TWIT GET', err);
+    }
+    data.forEach(tweet => {
+      if (!idStrings[tweet.id_str]) {
+        idStrings[tweet.id_str] = true;
+        let tweetObj = {};
+        tweetObj.user = tweet.user.screen_name;
+        tweetObj.text = tweet.text;
+        latestMentions.push(tweetObj);
+      } else {
+        console.log('Twitter Data', data);
+      }
+    });
+    console.log('ID STRINGS', idStrings);
+    console.log('LATEST MENTIONS', latestMentions);
+  });
+};
+
+// getMentions();
+
+
